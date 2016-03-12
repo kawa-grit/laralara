92bac2c63ef66a7ef12736c347cf0be7fe76a3c5
kawaguchi-ks <kawaguchi.gacco@gmail.com>
2016-03-12 15:04:24 +0900

    rename table

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -7,8 +7,8 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
-    'CreateProfilesTable' => $baseDir . '/database/migrations/2016_03_11_082005_create_profiles_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
+    'CreateUserOauthsTable' => $baseDir . '/database/migrations/2016_03_11_082005_create_user_oauths_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
     'File_Iterator' => $vendorDir . '/phpunit/php-file-iterator/src/Iterator.php',
     'File_Iterator_Facade' => $vendorDir . '/phpunit/php-file-iterator/src/Facade.php',
